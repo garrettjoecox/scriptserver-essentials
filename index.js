68cbbfdd1661e979f06d2d42e47c6f72352b97ae
Garrett Cox <garrettjcox@gmail.com>
2017-10-16 20:12:20 -0500

    Update for 1.0.0

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-'use strict';
-
 const _ = require('lodash');
 
-module.exports = function() {
-
+module.exports = function () {
   // Setup
-
   const server = this;
   const lastLocations = {};
   const tpRequests = {};
   const votes = {
     day: 0,
     night: 0,
-    weather: 0
+    weather: 0,
   };
   const voted = {
     day: {},
     night: {},
-    weather: {}
+    weather: {},
   };
 
-  server.use(require('scriptserver-event'));
-  server.use(require('scriptserver-command'));
-  server.use(require('scriptserver-json'));
-  server.use(require('scriptserver-util'));
+  server
+    .use(require('scriptserver-event'))
+    .use(require('scriptserver-command'))
+    .use(require('scriptserver-json'))
+    .use(require('scriptserver-util'));
 
   const config = server.config.essentials = _.defaultsDeep({}, server.config.essentials, {
     motd: {
       enabled: true,
       first: 'Welcome to the server, ${player}!',
-      text: 'Welcome back ${player}!'
+      text: 'Welcome back ${player}!',
     },
     starterKit: {
       enabled: true,
@@ -40,53 +37,50 @@ module.exports = function() {
         'iron_sword',
         'bed',
         'bread 32',
-      ]
+      ],
     },
     home: {
       enabled: true,
-      amount: 3
+      amount: 3,
     },
     spawn: true,
     warp: {
       enabled: true,
-      opOnly: true
+      opOnly: true,
     },
     tpa: true,
     back: true,
     day: {
       enabled: true,
-      percent: 50
+      percent: 50,
     },
     night: {
       enabled: true,
-      percent: 50
+      percent: 50,
     },
     weather: {
       enabled: true,
-      percent: 50
+      percent: 50,
     },
   });
 
   // Starter Kit & MOTD
-
   server.on('login', async (event) => {
     try {
       const hasJoined = await server.JSON.get(event.player, 'joined');
 
       if (hasJoined) {
         if (config.motd.enabled) await server.util.tellRaw(_.template(config.motd.text)(event), event.player, { color: 'yellow' });
-
       } else {
         await server.JSON.set(event.player, 'joined', Date.now());
 
         if (config.starterKit.enabled) await Promise.all(config.starterKit.items.map(i => server.send(`give ${event.player} minecraft:${i}`)));
         if (config.motd.enabled) await server.util.tellRaw(_.template(config.motd.first)(event), event.player, { color: 'yellow' });
       }
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // Home
-
   server.command('sethome', async (event) => {
     try {
       if (!config.home.enabled) throw new PlayerError('Homes are not enabled on this server');
@@ -99,16 +93,16 @@ module.exports = function() {
       if (homes[location.dimension].hasOwnProperty(home)) {
         homes[location.dimension][home] = location;
       } else {
-        if (Object.keys(homes[location.dimension]).length >= config.home.amount) {
-          throw new PlayerError(`Home limit hit, try deleting another home first`);
+        if (Object.keys(homes[location.dimension]).length >= config.home.amount) { // eslint-disable-line
+          throw new PlayerError('Home limit hit, try deleting another home first');
         } else {
           homes[location.dimension][home] = location;
         }
       }
 
       await server.JSON.set(event.player, 'home', homes);
-      await server.util.tellRaw(`Home${home === 'default' ? '' : ' ' + home} set in ${location.dimension}`, event.player, { color: 'gray'});
-    } catch(e) { handler(e, event.player); }
+      await server.util.tellRaw(`Home${home === 'default' ? '' : ' ' + home} set in ${location.dimension}`, event.player, { color: 'gray' });
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('delhome', async (event) => {
@@ -118,15 +112,15 @@ module.exports = function() {
       const location = await server.util.getLocation(event.player);
       const homes = await server.JSON.get(event.player, 'home');
 
-      if (!homes) throw new PlayerError(`You haven't set a home yet`);
+      if (!homes) throw new PlayerError('You haven\'t set a home yet');
       if (!homes.hasOwnProperty(location.dimension)) throw new PlayerError(`You haven't set a home in the ${location.dimension} yet`);
       if (!homes[location.dimension].hasOwnProperty(home)) throw new PlayerError(`You haven't set a home ${home} in the ${location.dimension}`);
 
       delete homes[location.dimension][home];
 
       await server.JSON.set(event.player, 'home', homes);
-      await server.util.tellRaw(`Home${home === 'default' ? '' : ' ' + home} removed in ${location.dimension}`, event.player, { color: 'gray'});
-    } catch(e) { handler(e, event.player); }
+      await server.util.tellRaw(`Home${home === 'default' ? '' : ' ' + home} removed in ${location.dimension}`, event.player, { color: 'gray' });
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('home', async (event) => {
@@ -136,7 +130,7 @@ module.exports = function() {
       const location = await server.util.getLocation(event.player);
       const homes = await server.JSON.get(event.player, 'home');
 
-      if (!homes) throw new PlayerError(`You haven't set a home yet`);
+      if (!homes) throw new PlayerError('You haven\'t set a home yet');
       if (!homes.hasOwnProperty(location.dimension)) throw new PlayerError(`You haven't set a home in the ${location.dimension} yet`);
       if (!homes[location.dimension].hasOwnProperty(home)) throw new PlayerError(`You haven't set a home ${home} in the ${location.dimension}`);
 
@@ -144,12 +138,10 @@ module.exports = function() {
       await server.send(`tp ${event.player} ${homes[location.dimension][home].x} ${homes[location.dimension][home].y} ${homes[location.dimension][home].z}`);
       await server.send(`execute ${event.player} ~ ~ ~ particle cloud ~ ~1 ~ 1 1 1 0.1 100 force`);
       await server.send(`playsound entity.item.pickup master ${event.player} ~ ~ ~ 10 1 1`);
-
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // Spawn
-
   server.command('setspawn', async (event) => {
     try {
       if (!config.spawn) throw new PlayerError('Spawn is not enabled on this server');
@@ -161,7 +153,7 @@ module.exports = function() {
 
       await server.JSON.set('world', 'spawn', spawns);
       await server.util.tellRaw(`${location.dimension} spawn set`, event.player, { color: 'gray' });
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('spawn', async (event) => {
@@ -176,11 +168,10 @@ module.exports = function() {
       await server.send(`tp ${event.player} ${spawns[location.dimension].x} ${spawns[location.dimension].y} ${spawns[location.dimension].z}`);
       await server.send(`execute ${event.player} ~ ~ ~ particle cloud ~ ~1 ~ 1 1 1 0.1 100 force`);
       await server.send(`playsound entity.item.pickup master ${event.player} ~ ~ ~ 10 1 1`);
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // Warp
-
   server.command('setwarp', async (event) => {
     try {
       if (!config.warp.enabled) throw new PlayerError('Warps are not enabled on this server');
@@ -188,14 +179,14 @@ module.exports = function() {
       if (!event.args[0]) throw new PlayerError('Please provide a name for the warp');
       const warp = event.args[0];
       const location = await server.util.getLocation(event.player);
-      let warps = await server.JSON.get('world', 'warp') || {};
+      const warps = await server.JSON.get('world', 'warp') || {};
 
       warps[location.dimension] = warps[location.dimension] || {};
       warps[location.dimension][warp] = location;
 
       await server.JSON.set('world', 'warp', warps);
-      await server.util.tellRaw(`Warp ${warp} set in ${location.dimension}`, event.player, { color: 'gray'});
-    } catch(e) { handler(e, event.player); }
+      await server.util.tellRaw(`Warp ${warp} set in ${location.dimension}`, event.player, { color: 'gray' });
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('delwarp', async (event) => {
@@ -208,15 +199,15 @@ module.exports = function() {
       const location = await server.util.getLocation(event.player);
       const warps = await server.JSON.get('world', 'warp');
 
-      if (!warps) throw new PlayerError(`You haven't set a warp yet`);
+      if (!warps) throw new PlayerError('You haven\'t set a warp yet');
       if (!warps.hasOwnProperty(location.dimension)) throw new PlayerError(`You haven't set a warp in the ${location.dimension} yet`);
       if (!warps[location.dimension].hasOwnProperty(warp)) throw new PlayerError(`You haven't set a warp ${warp} in the ${location.dimension}`);
 
       delete warps[location.dimension][warp];
 
       await server.JSON.set('world', 'warp', warps);
-      await server.util.tellRaw(`Warp ${warp} removed in ${location.dimension}`, event.player, { color: 'gray'});
-    } catch(e) { handler(e, event.player); }
+      await server.util.tellRaw(`Warp ${warp} removed in ${location.dimension}`, event.player, { color: 'gray' });
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('warp', async (event) => {
@@ -225,10 +216,10 @@ module.exports = function() {
       const location = await server.util.getLocation(event.player);
       const warps = await server.JSON.get('world', 'warp');
 
-      if (!warps) throw new PlayerError(`No warps have been created yet`);
+      if (!warps) throw new PlayerError('No warps have been created yet');
       if (!warps.hasOwnProperty(location.dimension)) throw new PlayerError(`No warps have been created in the ${location.dimension}`);
       if (!event.args[0]) {
-        let warpList = Object.keys(warps[location.dimension]).join(', ');
+        const warpList = Object.keys(warps[location.dimension]).join(', ');
 
         await server.util.tellRaw(`Warps available in the ${location.dimension}: ` + warpList, event.player, { color: 'gray' });
       } else {
@@ -240,11 +231,10 @@ module.exports = function() {
         await server.send(`execute ${event.player} ~ ~ ~ particle cloud ~ ~1 ~ 1 1 1 0.1 100 force`);
         await server.send(`playsound entity.item.pickup master ${event.player} ~ ~ ~ 10 1 1`);
       }
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // TPA
-
   server.command('tpa', async (event) => {
     try {
       if (!config.tpa) throw new PlayerError('Teleport requests are not enabled on this server');
@@ -259,12 +249,12 @@ module.exports = function() {
       tpRequests[toPlayer.toLowerCase()] = {
         type: 'tpa',
         player: fromPlayer,
-        timestamp: Date.now()
+        timestamp: Date.now(),
       };
 
       await server.util.tellRaw(`tpa sent to ${toPlayer}, expires in 2 minutes`, fromPlayer, { color: 'gray' });
       await server.util.tellRaw(`tpa received from ${fromPlayer}, use ~tpaccept or ~tpdeny, expires in 2 minutes`, toPlayer, { color: 'gray' });
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('tpahere', async (event) => {
@@ -281,12 +271,12 @@ module.exports = function() {
       tpRequests[toPlayer.toLowerCase()] = {
         type: 'tpahere',
         player: fromPlayer,
-        timestamp: Date.now()
+        timestamp: Date.now(),
       };
 
       await server.util.tellRaw(`tpahere sent to ${toPlayer}, expires in 2 minutes`, fromPlayer, { color: 'gray' });
       await server.util.tellRaw(`tpahere received from ${fromPlayer}, use ~tpaccept or ~tpdeny, expires in 2 minutes`, toPlayer, { color: 'gray' });
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('tpdeny', async (event) => {
@@ -297,8 +287,8 @@ module.exports = function() {
       delete tpRequests[event.player.toLowerCase()];
 
       await server.util.tellRaw(`${req.type} to ${event.player} denied`, req.player, { color: 'gray' });
-      await server.util.tellRaw(`${req.type} from ${req.player} denied`, event.player, { color: 'gray'});
-    } catch(e) { handler(e, event.player); }
+      await server.util.tellRaw(`${req.type} from ${req.player} denied`, event.player, { color: 'gray' });
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('tpaccept', async (event) => {
@@ -332,11 +322,10 @@ module.exports = function() {
       await server.send(req.command);
       await server.send(`execute ${req.type === 'tpa' ? f.player : t.player} ~ ~ ~ particle cloud ~ ~1 ~ 1 1 1 0.1 100 force`);
       await server.send(`playsound entity.item.pickup master ${req.type === 'tpa' ? f.player : t.player} ~ ~ ~ 10 1 1`);
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // Back
-
   server.command('back', async (event) => {
     try {
       if (!config.back) throw new PlayerError('Back is not enabled on this server');
@@ -350,11 +339,10 @@ module.exports = function() {
       await server.send(`tp ${event.player} ${lastLoc.x} ${lastLoc.y} ${lastLoc.z}`);
       await server.send(`execute ${event.player} ~ ~ ~ particle cloud ~ ~1 ~ 1 1 1 0.1 100 force`);
       await server.send(`playsound entity.item.pickup master ${event.player} ~ ~ ~ 10 1 1`);
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   // Votes
-
   server.command('day', async (event) => {
     try {
       if (!config.day.enabled) throw new PlayerError('Day votes are not enabled on this server');
@@ -362,7 +350,7 @@ module.exports = function() {
 
       if (!voted.day.hasOwnProperty(event.player)) {
         voted.day[event.player] = true;
-        votes.day++;
+        votes.day += 1;
       }
 
       if (votes.day >= (amount * (config.day.percent / 100))) {
@@ -373,7 +361,7 @@ module.exports = function() {
       } else if (votes.day === 1) {
         await server.util.tellRaw('Day vote started, use ~day to vote!', '@a', { color: 'gray' });
       }
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('night', async (event) => {
@@ -383,7 +371,7 @@ module.exports = function() {
 
       if (!voted.night.hasOwnProperty(event.player)) {
         voted.night[event.player] = true;
-        votes.night++;
+        votes.night += 1;
       }
 
       if (votes.night >= (amount * (config.night.percent / 100))) {
@@ -394,7 +382,7 @@ module.exports = function() {
       } else if (votes.night === 1) {
         await server.util.tellRaw('Night vote started, use ~night to vote!', '@a', { color: 'gray' });
       }
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   server.command('weather', async (event) => {
@@ -404,7 +392,7 @@ module.exports = function() {
 
       if (!voted.weather.hasOwnProperty(event.player)) {
         voted.weather[event.player] = true;
-        votes.weather++;
+        votes.weather += 1;
       }
 
       if (votes.weather >= (amount * (config.weather.percent / 100))) {
@@ -415,13 +403,16 @@ module.exports = function() {
       } else if (votes.weather === 1) {
         await server.util.tellRaw('Weather vote started, use ~weather to vote!', '@a', { color: 'gray' });
       }
-    } catch(e) { handler(e, event.player); }
+    } catch (e) { handler(e, event.player); }
   });
 
   function handler(e, player) {
-    if (e.playerError) server.util.tellRaw(e.message, player, { color: 'red' });
-    else server.util.tellRaw('Something went wrong, please let the server admin know of the following error:', player, { color: 'red' })
-      .then(() => server.util.tellRaw(e.message, player, { color: 'gray' }));
+    if (e.playerError) {
+      server.util.tellRaw(e.message, player, { color: 'red' });
+    } else {
+      server.util.tellRaw('Something went wrong, please let the server admin know of the following error:', player, { color: 'red' })
+        .then(() => server.util.tellRaw(e.message, player, { color: 'gray' }));
+    }
   }
 
   class PlayerError extends Error {
@@ -430,5 +421,4 @@ module.exports = function() {
       this.playerError = true;
     }
   }
-
-}
+};
